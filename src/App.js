389a923d76bd01775bab058cde51e0b33ebd9068
noor traders <lnoortraders@gmail.com>
2023-09-14 22:46:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import { ToastContainer, toast } from 'react-toastify';
 import './App.css';
 import { PUBLIC_ADMIN_EMAIL_HASH, PUBLIC_ADMIN_NAME_HASH, PUBLIC_LOGGEDIN_HASH } from './contastans/constant';
 import { AppContext } from './context/appContext';
-import {login} from './features/admin/adminSlice'
-import {Company} from './Components';
-import { Cart, Category, ContactUs, FilteredProducts, Footer, Hero, Navbar, NotFound, OrderForm, Shop ,AddProducts,DashBoard,AdminLogin,EditProduct, Orders, OrderPage} from './Components';
+import { login } from './features/admin/adminSlice'
+import { Company } from './Components';
+import { Cart, Category, ContactUs, FilteredProducts, Footer, Hero, Navbar, NotFound, OrderForm, Shop, AddProducts, DashBoard, AdminLogin, EditProduct, Orders, OrderPage } from './Components';
 
 
 
 function App() {
   const dispatch = useDispatch();
-  const showToast = (msg, isError=false) => {
+  const showToast = (msg, isError = false) => {
     if (!isError) {
       toast.success(msg, {
         position: "top-left",
@@ -27,7 +27,7 @@ function App() {
         theme: "dark",
       });
     }
-    else{
+    else {
       toast.error(msg, {
         position: "top-left",
         autoClose: 4000,
@@ -38,59 +38,59 @@ function App() {
         progress: undefined,
         theme: "dark",
       });
-    }  
-     // toast('this is message');
+    }
+    // toast('this is message');
   }
   useEffect(() => {
-    let isLoggedIn=localStorage.getItem(PUBLIC_LOGGEDIN_HASH);
-    console.log({isLoggedIn});
-    if(isLoggedIn){
-      let adminName=localStorage.getItem(PUBLIC_ADMIN_NAME_HASH);
-      let admin_email=localStorage.getItem(PUBLIC_ADMIN_EMAIL_HASH);
-      console.log({adminName});
-      dispatch(login({admin_name:adminName,admin_email:admin_email}))
+    let isLoggedIn = localStorage.getItem(PUBLIC_LOGGEDIN_HASH);
+    console.log({ isLoggedIn });
+    if (isLoggedIn) {
+      let adminName = localStorage.getItem(PUBLIC_ADMIN_NAME_HASH);
+      let admin_email = localStorage.getItem(PUBLIC_ADMIN_EMAIL_HASH);
+      console.log({ adminName });
+      dispatch(login({ admin_name: adminName, admin_email: admin_email }))
     }
 
   },)
-  
+
   return (
-    <AppContext.Provider value={{showToast}} >
-    <div className='relative'  >
-      <Navbar showToast={showToast} />
-      <ToastContainer
-        position="bottom-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <Routes >
-        <Route path='/' element={<Hero />} />
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/category' element={<Category />} />
-        <Route path='/company' element={<Company />} />
-        <Route path='/contact-us' element={<ContactUs />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/cart/order-now' element={<OrderForm />} />
-        {/* <Route path='/add-product' element={<AddProducts />} /> */}
-        <Route path='/category/:campany_name' element={<FilteredProducts queryParameter={'product_category'} />} />
-        <Route path='/company/:campany_name' element={<FilteredProducts queryParameter={'product_company'} />} />
-        <Route path='/admin/add-product' element={<AddProducts />} />
-        <Route path='/admin/login' element={<AdminLogin />} />
-        <Route path='/admin/dashboard' element={<DashBoard />} />
-        <Route path='/admin/edit-product/:product_id' element={<EditProduct />} />
-        <Route path='/admin/orders' element={<Orders />} />
-        <Route path='/admin/orders/:order_id' element={<OrderPage />} />
-        {/* <Route path='/admin/file-reader' element={<FileReaderCSV />} /> */}
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
+    <AppContext.Provider value={{ showToast }} >
+      <div className='relative'  >
+        <Navbar showToast={showToast} />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <Routes>
+          <Route path='/' element={<Hero />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/category' element={<Category />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/cart/order-now' element={<OrderForm />} />
+          {/* <Route path='/add-product' element={<AddProducts />} /> */}
+          <Route path='/category/:campany_name' element={<FilteredProducts queryParameter={'product_category'} />} />
+          <Route path='/company/:campany_name' element={<FilteredProducts queryParameter={'product_company'} />} />
+          <Route path='/admin/add-product' element={<AddProducts />} />
+          <Route path='/admin/login' element={<AdminLogin />} />
+          <Route path='/admin/dashboard' element={<DashBoard />} />
+          <Route path='/admin/edit-product/:product_id' element={<EditProduct />} />
+          <Route path='/admin/orders' element={<Orders />} />
+          <Route path='/admin/orders/:order_id' element={<OrderPage />} />
+          {/* <Route path='/admin/file-reader' element={<FileReaderCSV />} /> */}
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </div>
     </AppContext.Provider>
 
   );
